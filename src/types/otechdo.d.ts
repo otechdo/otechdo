91929e22a6829cbb0b4c200228c75b8d27e79439
Willy Micieli <otechdo@otechdo.com>
2024-11-02 07:12:32 +0100

    clean code

--- src/types/otechdo.d.ts
+++ src/types/otechdo.d.ts
@@ -2,6 +2,15 @@ import { Otechdo } from "../otechdo";
 
 declare global {
     /**
+     * Select an element on success
+     * to manipulate it
+     *
+     * @param selector `string`
+     * @returns `Otechdo` instance
+     */
+    export function o(selector: string): Otechdo;
+
+    /**
      * Manipulate the element passed in parameter
      *
      * @param e `HTMLElement` - The element to manipulate
